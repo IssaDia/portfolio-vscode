261f5a19f37ac11d501698c4ff90c1cf4991613a
IssaDia <44264590+IssaDia@users.noreply.github.com>
2024-02-01 15:52:05 +0100

    minor changes for deployment part 2

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -2,19 +2,33 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { EmailTemplate } from "../../components/contact/EmailTemplate";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.NEXT_RESEND_API_KEY);
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { data, error } = await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to: ["delivered@resend.dev"],
-    subject: "Hello world",
-    react: EmailTemplate({ firstName: "John" }),
-  });
-
-  if (error) {
-    return res.status(400).json(error);
+const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (!process.env.RESEND_API_KEY) {
+    console.error("RESEND_API_KEY is not defined.");
+    return res.status(500).json({ message: "Server configuration error." });
   }
 
-  res.status(200).json(data);
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  try {
+    const { name, email, subject, content } = req.body;
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: [email],
+      subject: subject,
+      react: EmailTemplate({ firstName: name }),
+    });
+
+    if (error) {
+      console.error(error);
+      return res.status(400).json(error);
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error sending email." });
+  }
 };
+
+export default sendEmail;
