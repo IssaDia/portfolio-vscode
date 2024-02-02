601f37f40d6905173858455660c6a581136a0354
IssaDia <44264590+IssaDia@users.noreply.github.com>
2024-02-02 12:57:34 +0100

    added border bottom on topbar

--- components/topbar/Topbar.tsx
+++ components/topbar/Topbar.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./searchBar/SearchBar";
 
 const Topbar = () => {
   return (
-    <div className=" bg-topbar-background flex md:flex-row w-full">
+    <div className=" bg-topbar-background flex md:flex-row w-full border-b-0.5 border-opacity-80 border-main-background">
       <div className="phone:w-2/12 ipadPortrait:w-1/12">
         <Buttons />
       </div>
--- widget/TopMenuItem.tsx
+++ widget/TopMenuItem.tsx
@@ -3,34 +3,48 @@ import MenuItemI from "../lib/interfaces/MenuItemI";
 import Link from "next/link";
 import ActiveLink from "./activeLink/ActiveLink";
 import Image from "next/image";
+import { faTimes, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const TopMenuItem = ({ image, name, path }: MenuItemI) => {
+  const [isHovered, setIsHovered] = React.useState(false);
+
   return (
     <ActiveLink href={path} activeClassName="active">
       {(isActive: boolean) => (
         <div
-          className={`flex flex-row justify-start w-full p-2 cursor-pointer border-2 border-opacity-10 border-black  hover:bg-main-background ${
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          className={`flex flex-row justify-between w-full p-2 cursor-pointer  hover:bg-main-background  ${
             isActive
               ? "bg-black"
-              : "bg-sidebar-background phone:hidden  ipadLandscape:block"
+              : "bg-sidebar-background phone:hidden ipadLandscape:block"
           } `}
         >
-          <Link href={path} legacyBehavior passHref>
-            <a className="block  w-full h-full ">
-              <div className="flex flex-row phone:justify-start">
-                <Image
-                  src={`/images/${image}.svg`}
-                  alt="nav-items-logo"
-                  width={18}
-                  height={18}
-                  className="mr-2"
-                />
-                <p className="text-sm brightness-150 text-menu-firstVariant">
-                  {name}
-                </p>
-              </div>
-            </a>
-          </Link>
+          <div>
+            <Link href={path} legacyBehavior passHref>
+              <a className="block  w-full h-full ">
+                <div className="flex flex-row phone:justify-start">
+                  <Image
+                    src={`/images/${image}.svg`}
+                    alt="nav-items-logo"
+                    width={18}
+                    height={18}
+                    className="mr-2"
+                  />
+                  <p className="text-sm brightness-150 text-menu-firstVariant">
+                    {name}
+                  </p>
+                  {/* {isHovered && (
+                    <FontAwesomeIcon
+                      icon={faXmark}
+                      className="text-sidebar-background  cursor-pointer hidden ipadLandscape:block mr-12"
+                    />
+                  )} */}
+                </div>
+              </a>
+            </Link>
+          </div>
         </div>
       )}
     </ActiveLink>
