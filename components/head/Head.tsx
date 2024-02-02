3ee69d736343667638dbfe3f16eca68f07db97f8
IssaDia <44264590+IssaDia@users.noreply.github.com>
2024-02-02 21:05:47 +0100

    removed unused meta

--- components/head/Head.tsx
+++ components/head/Head.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 type CustomHeadProps = {
-  title: string
-}
+  title: string;
+};
 
 const CustomHead = ({ title }: CustomHeadProps) => {
   return (
@@ -10,26 +10,19 @@ const CustomHead = ({ title }: CustomHeadProps) => {
       <title>{title}</title>
       <meta
         name="description"
-        content="Issa Dia is an avid full stack web developer building websites and applications you'd love to use"
+        content="Issa Dia is a passionate front end web developer that builds websites and applications"
       />
       <meta
         name="keywords"
-        content="issa dia, issa, dia, web developer portfolio, issa web developer, issa developer, issa dia portfolio, vscode-portfolio"
+        content="issa dia, issa, dia, web developer portfolio, issa web developer, issa frontend developer, issa dia portfolio, vscode-portfolio"
       />
       <meta property="og:title" content="Issa Dia's Portfolio" />
-      <meta
-        property="og:description"
-        content="A full-stack developer building websites that you'd like to use."
-      />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
-      <meta name="twitter:card" content="summary_large_image" />
     </Head>
-  )
-}
+  );
+};
 
-export default CustomHead
+export default CustomHead;
 
 CustomHead.defaultProps = {
-  title: 'Issa Dia',
-}
+  title: "Issa Dia",
+};
